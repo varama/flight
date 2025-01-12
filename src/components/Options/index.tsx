6edd0ba7fda1ddf6c5f89872a9c68545f03cd7f6
Davit Varamashvili <varamashvili@nomachine.com>
2025-01-12 16:09:03 +0400

    Finished trip type and flight class part

--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,4 +1,7 @@
+import useClickDetection from "hooks/useClickDetection";
+import React from "react";
 import { FLIGHTCLASSES, FLIGHTTYPES } from "utils/enums";
+import styles from "./style.module.scss";
 
 interface OptionsProps {
   value: string;
@@ -6,8 +9,65 @@ interface OptionsProps {
   onSelect: (option: FLIGHTTYPES | FLIGHTCLASSES) => void;
 }
 
-const Options: React.FC<OptionsProps> = () => {
-  return <div></div>;
+const Options: React.FC<OptionsProps> = ({ value, options, onSelect }) => {
+  const optionsRef = React.useRef(null);
+  const [showDropdown, setShowDropdown] = React.useState(false);
+  useClickDetection(optionsRef, () => {
+    setShowDropdown(false);
+  });
+  return (
+    <div ref={optionsRef} style={{ width: "fit-content" }}>
+      <div
+        className={styles.title}
+        onClick={() => {
+          setShowDropdown(!showDropdown);
+        }}
+      >
+        {value}
+        <svg
+          viewBox="7 10 10 5"
+          focusable="false"
+          style={{ width: 10, height: 10 }}
+        >
+          <polygon
+            stroke="none"
+            fillRule="evenodd"
+            points="7 10 12 15 17 10"
+          ></polygon>
+        </svg>
+      </div>
+      {showDropdown && (
+        <div className={styles.dropdown}>
+          {options.map((option) => {
+            return (
+              <div
+                className={value === option ? styles.active : ""}
+                key={option}
+                onClick={() => {
+                  onSelect(option);
+                  setShowDropdown(false);
+                }}
+              >
+                {value === option ? (
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    focusable="false"
+                  >
+                    <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"></path>
+                  </svg>
+                ) : (
+                  <div style={{ width: 20 }}></div>
+                )}
+                {option}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Options;
